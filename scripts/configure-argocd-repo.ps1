194fa8975d547ecd7b4ddb275a6750aab021ed15
Umesh Chhabra <umeshchhabra@gmail.com>
2026-08-28 00:40:20 -0500

    Prompt securely for Argo CD repository token

--- scripts/configure-argocd-repo.ps1
+++ scripts/configure-argocd-repo.ps1
@@ -1,10 +1,15 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory)]
-    [string]$GitHubToken
+    [securestring]$GitHubToken
 )
 
 $ErrorActionPreference = 'Stop'
+if (-not $GitHubToken) {
+    $GitHubToken = Read-Host -Prompt 'GitHub fine-grained token (read access to mlops-foundry)' -AsSecureString
+}
+$tokenPointer = [Runtime.InteropServices.Marshal]::SecureStringToBSTR($GitHubToken)
+try {
+    $tokenValue = [Runtime.InteropServices.Marshal]::PtrToStringBSTR($tokenPointer)
 $secret = @{
     apiVersion = 'v1'
     kind = 'Secret'
@@ -17,9 +22,15 @@ $secret = @{
         type = 'git'
         url = 'https://github.com/umeshchhabra/mlops-foundry.git'
         username = 'x-access-token'
-        password = $GitHubToken
+        password = $tokenValue
     }
 } | ConvertTo-Json -Depth 6
 
 $secret | kubectl apply -f -
 if ($LASTEXITCODE -ne 0) { throw 'Failed to configure the Argo CD repository credential.' }
+}
+finally {
+    if ($tokenPointer -ne [IntPtr]::Zero) {
+        [Runtime.InteropServices.Marshal]::ZeroFreeBSTR($tokenPointer)
+    }
+}
